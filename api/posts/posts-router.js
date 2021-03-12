98b44ed41288b985c5a99f0ddf14f79ead013f1c
ChadDiaz <chadcdiaz@gmail.com>
2021-03-12 15:08:24 -0500

    first endpoint complete

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,14 @@
 // implement your posts router here
+const router = require("express").Router();
+const Post = require("./posts-model.js");
+
+router.get("/", async (req, res) => {
+  try {
+    const post = await Post.find();
+    res.status(200).json(post);
+  } catch (err) {
+      res.status(500).json({message: "The posts information could not be retrieved"})
+  }
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,2 +1,11 @@
 // implement your server here
+const express = require('express');
+const server = express();
+
+
 // require your posts router and connect it here
+const PostRouter = require('./posts/posts-router.js');
+server.use(express.json());
+server.use('/api/post', PostRouter);
+
+module.exports = server;
