a226788688c200ff6b21193fdb5254a629d1ecfe
ORiveraJr84 <oriverajr84@gmail.com>
2021-03-15 11:45:07 -0400

    Server set up and initial posts get complete.

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,17 @@
 // implement your posts router here
+const express = require("express");
+const postsRouter = express.Router();
+const posts = require("./posts-model");
+
+postsRouter.get("/", async (req, res) => {
+  try {
+    const post = await posts.find();
+    res.status(200).json(post);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "The posts information could not be retrieved" });
+  }
+});
+
+module.exports = postsRouter;
